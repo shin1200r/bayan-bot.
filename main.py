45d54de17a583419d9a905515f1e9d4f72ae1eea
shin1200r <ichiroyamada607@gmail.com>
2026-06-27 21:06:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import telebot
 import os
 from flask import Flask, request
+from telebot import types # Не забудь этот импорт!
 
 TOKEN = '8201596025:AAHi7UUJdAr6EWX6JiQAISrnaDsrDHRPvWA'
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-# Путь для приема сообщений от Telegram
+# --- ЛОГИКА БОТА ---
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка 1")
+    item2 = types.KeyboardButton("Кнопка 2")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, "Привет! Я работаю через Webhook! Выбери что-то:", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Ты написал: {message.text}")
+
+# --- WEBHOOK ---
 @app.route('/' + TOKEN, methods=['POST'])
 def get_message():
-    print("Пришло сообщение!") 
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
