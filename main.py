029cfe2d4958b8bb5a40bde26a03e89011c79d8c
shin1200r <ichiroyamada607@gmail.com>
2026-06-27 21:05:37 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ TOKEN = '8201596025:AAHi7UUJdAr6EWX6JiQAISrnaDsrDHRPvWA'
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-# Этот путь принимает обновления от Telegram
+# Путь для приема сообщений от Telegram
 @app.route('/' + TOKEN, methods=['POST'])
 def get_message():
+    print("Пришло сообщение!") 
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
@@ -18,7 +19,6 @@ def get_message():
 def home():
     return "Bot is alive"
 
-# ВАЖНО: Выполняется один раз для установки адреса вебхука
 def set_webhook():
     webhook_url = f"https://bayan-bot-web.onrender.com/{TOKEN}"
     bot.remove_webhook()
